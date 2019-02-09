06bc444b4542c3e6953b30c79c75ae6da815241b
Vincent <m@finzzz.net>
2019-02-09 13:17:21 +0900

    fix filepath error

--- color_hist.py
+++ color_hist.py
@@ -8,26 +8,31 @@ from matplotlib import pyplot as plt  # noqa : E402
 
 
 parser = argparse.ArgumentParser()
-parser.add_argument('-i', '--image', nargs=2, help="true fake")
+parser.add_argument('-i', '--image', help="true fake")
 parser.add_argument('-c', '--channel', choices=["r", "g", "b", "all"],
                     default="all")
 args = parser.parse_args()
 
 try:
-    for _ in args.image:
-        filename = path.basename(_)
-        dirname = path.dirname(_)
-        if " " in filename or " " in dirname:
-            raise Exception("file/dir name must not contain space")
+    filename = path.basename(args.image)
+    dirname = path.dirname(args.image)
+    if " " in filename or " " in dirname:
+        raise Exception("file/dir name must not contain space")
 except Exception as e:
     print(f"{e}, exitting...")
     sys.exit()
 
+num = filename[2]
+true_img = path.join(dirname, f"{num}_truth.png")
+input_img = path.join(dirname, f"{num}_x.png")
+
 # "res 70%/1_truth.png"
-true = cv2.imread(args.image[0])
-fake = cv2.imread(args.image[1])
+true = cv2.imread(true_img)
+fake = cv2.imread(args.image)
+inp = cv2.imread(input_img)
 color_dict = {"b": 'turquoise', "g": 'lightgreen', "r": 'tomato'}
-fake_color_dict = {"b": "indigo", "g": "green", "r": "magenta"}
+color_dict = {"b": 'darkblue', "g": 'darkgreen', "r": 'crimson'}
+fake_color_dict = {"b": "indigo", "g": "green", "r": "red"}
 if args.channel == "all":
     color = []
     fake_color = []
@@ -44,8 +49,14 @@ for i, col in enumerate(color):
     plt.plot(histr, color=col, linestyle='--')
     plt.xlim([0, 256])
 
+for i, col in enumerate(color):
+    histr = cv2.calcHist([inp], [i], None, [256], [0, 256])
+    plt.plot(histr, color=col, linestyle='dotted')
+    plt.xlim([0, 256])
+
 for i, col in enumerate(fake_color):
     histr = cv2.calcHist([fake], [i], None, [256], [0, 256])
     plt.plot(histr, color=col)
     plt.xlim([0, 256])
+
 plt.show()
